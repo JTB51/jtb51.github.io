6079b4dcaa9a9d5001abc87563b318977921d60f
jt burgoyne <jt.burgoyne08@gmail.com>
2026-03-19 17:16:38 +0000

    got to factory function part of the readme

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,13 +10,44 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
+  const PLAYER_WIDTH = $(".player").width();
+  const PLAYER_HEIGHT = $(".player").height();
   
+  const KEY = {
+    UP: 38, // right player 
+    DOWN: 40,
+
+    W: 87, // left player
+    S: 83, 
+
+    SPACE: 32,
+    ENTER: 13,
+  }
   // Game Item Objects
 
+    let player1 = {
+    id: "#player1",
+    x: 0,
+    y: 0,
+    speedX: 0,
+    speedY: 0
+  }
+  
+  let player2 = {
+    id: "#player2",
+    x: BOARD_WIDTH - PLAYER_WIDTH,
+    y: BOARD_HEIGHT - PLAYER_HEIGHT,
+    speedX: 0,
+    speedY: 0
+  }
+  
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);
+  $(document).on('keyup', handleKeyUp);                          
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -42,6 +73,32 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function handleKeyDown(event) {
+    if (event.which === KEY.UP) {
+        player2.speedY = -5;
+        player2.speedX = 0;
+    } else if (event.which === KEY.DOWN) {
+        player2.speedY = 5;
+        player2.speedX = 0;
+    }
+    if (event.which === KEY.W) {
+        player1.speedY = -5;
+        player1.speedX = 0;
+    } else if (event.which === KEY.S) {
+        player1.speedY = 5;
+        player1.speedX = 0;
+    }
+  }
+
+  function handleKeyUp (){
+    if (event.which === KEY.W || event.which === KEY.S) {
+      player1.speedY = 0;
+    }
+
+    if (event.which === KEY.UP || event.which === KEY.DOWN) {
+      player2.speedY = 0;
+    }
+  }
   
   function endGame() {
     // stop the interval timer
@@ -50,5 +107,4 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
 }
